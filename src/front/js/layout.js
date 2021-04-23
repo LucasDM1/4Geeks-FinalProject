95254a2439da7faadc72f4ac84889ba01ddcff14
María Celeste Alpízar Solano <maria.alpizar.72@gmail.com>
2021-04-23 16:18:29 +0000

    Ads ready

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,6 +9,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Carousel } from "./component/carousel";
 
 //create your first component
 const Layout = () => {
@@ -23,6 +24,7 @@ const Layout = () => {
 					<Navbar />
 					<Switch>
 						<Route exact path="/">
+							<Carousel />
 							<Home />
 						</Route>
 						<Route exact path="/demo">
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,265 +1,273 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+// import "bootstrap/dist/css/bootstrap.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="container-fluid">
-			<div className="row justify-content-center">
-				<div className="col-10">
-					<div
-						id="carouselExampleIndicators"
-						className="carousel slide"
-						data-bs-ride="carousel"
-						style={{ backgroundColor: "gray" }}>
-						<ol className="carousel-indicators">
-							<li
-								data-target="#carouselExampleIndicators"
-								data-slide-to="0"
-								className="active"
-								style={{
-									borderRadius: "50%",
-									width: "20px",
-									height: "20px",
-									backgroundColor: "black",
-									opacity: "0.25"
-								}}
-							/>
-							<li
-								data-target="#carouselExampleIndicators"
-								data-slide-to="1"
-								style={{
-									borderRadius: "50%",
-									width: "20px",
-									height: "20px",
-									backgroundColor: "black",
-									opacity: "0.25"
-								}}
-							/>
-							<li
-								data-target="#carouselExampleIndicators"
-								data-slide-to="2"
-								style={{
-									borderRadius: "50%",
-									width: "20px",
-									height: "20px",
-									backgroundColor: "black",
-									opacity: "0.25"
-								}}
-							/>
-						</ol>
-						<div className="carousel-inner">
-							<div className="carousel-item active">
-								<div className="container" alt="First slide">
-									<div className="row p-1">
-										<div className="col-sm-12 col-lg-3 ">
-											<img
-												src="https://picsum.photos/250"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title text-center">Card title</h5>
-											</div>
-										</div>
-										<div className="col-sm-12 col-lg-3 ">
-											<img
-												src="https://picsum.photos/250"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title text-center">Card title</h5>
-											</div>
-										</div>
-										<div className="col-sm-12 col-lg-3 ">
-											<img
-												src="https://picsum.photos/250"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title text-center">Card title</h5>
-											</div>
-										</div>
-										<div className="col-sm-12 col-lg-3 ">
-											<img
-												src="https://picsum.photos/250"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title text-center">Card title</h5>
-											</div>
-										</div>
-									</div>
-								</div>
+			<div className="row m-4">
+				<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+					<div className="card mb-3" style={{ maxwidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src="https://picsum.photos/200/200" style={{ width: "200px", height: "200px" }} />
 							</div>
-							<div className="carousel-item">
-								<div className="container" alt="Second slide">
-									<div className="row p-1">
-										<div className="col-sm-12 col-lg-3 ">
-											<img
-												src="https://neilpatel.com/wp-content/uploads/2017/08/colors.jpg"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title">Card title</h5>
-											</div>
-										</div>
-										<div className="col-sm-12 col-lg-3 ">
-											<img
-												src="https://neilpatel.com/wp-content/uploads/2017/08/colors.jpg"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title">Card title</h5>
-											</div>
-										</div>
-										<div className="col-sm-12 col-lg-3 ">
-											<img
-												src="https://neilpatel.com/wp-content/uploads/2017/08/colors.jpg"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title">Card title</h5>
-											</div>
-										</div>
-										<div className="col-sm-12 col-lg-3 ">
-											<img
-												src="https://neilpatel.com/wp-content/uploads/2017/08/colors.jpg"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title">Card title</h5>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="carousel-item">
-								<div className="container">
-									<div className="row p-1">
-										<div className="col-sm-12 col-lg-3">
-											<img
-												src="https://www.hola.com/imagenes/cocina/recetas/20201009176964/pan-de-leche-thermomix-casero/0-876-239/receta-pan-thermomix-bollo-leche-m.jpg"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title text-dark text-center">Card title</h5>
-											</div>
-										</div>
-										<div className="col-sm-12 col-lg-3">
-											<img
-												src="https://www.hola.com/imagenes/cocina/recetas/20201009176964/pan-de-leche-thermomix-casero/0-876-239/receta-pan-thermomix-bollo-leche-m.jpg"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title text-dark text-center">Card title</h5>
-											</div>
-										</div>
-										<div className="col-sm-12 col-lg-3">
-											<img
-												src="https://www.hola.com/imagenes/cocina/recetas/20201009176964/pan-de-leche-thermomix-casero/0-876-239/receta-pan-thermomix-bollo-leche-m.jpg"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title text-dark text-center">Card title</h5>
-											</div>
-										</div>
-										<div className="col-sm-12 col-lg-3">
-											<img
-												src="https://www.hola.com/imagenes/cocina/recetas/20201009176964/pan-de-leche-thermomix-casero/0-876-239/receta-pan-thermomix-bollo-leche-m.jpg"
-												className="card-img"
-												style={{
-													width: "250px",
-													height: "250px"
-												}}
-											/>
-											<div className="card-img-overlay">
-												<h5 className="card-title text-dark text-center">Card title</h5>
-											</div>
-										</div>
-									</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">
+										<b>Nombre del servicio</b>
+									</h5>
+									<p>
+										<b>Categoria</b>
+									</p>
+									<p>
+										<b>Ubicacion</b>
+									</p>
+									<p className="card-text">
+										<button type="button" className="btn btn-outline-dark">
+											More information
+										</button>
+									</p>
 								</div>
 							</div>
 						</div>
-						<a
-							className="carousel-control-prev"
-							role="button"
-							href="#carouselExampleIndicators"
-							data-slide="prev"
-							style={{ width: "40px" }}>
-							<span className="carousel-control-prev-icon" aria-hidden="true" />
-							<span className="visually-hidden" />
-						</a>
-						<a
-							className="carousel-control-next"
-							role="button"
-							href="#carouselExampleIndicators"
-							data-slide="next"
-							style={{ width: "40px" }}>
-							<span className="carousel-control-next-icon" aria-hidden="true" />
-							<span className="visually-hidden" />
-						</a>
+					</div>
+				</div>
+				<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+					<div className="card mb-3" style={{ maxwidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src="https://picsum.photos/200/200" style={{ width: "200px", height: "200px" }} />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">
+										<b>Nombre del servicio</b>
+									</h5>
+									<p>
+										<b>Categoria</b>
+									</p>
+									<p>
+										<b>Ubicacion</b>
+									</p>
+									<p className="card-text">
+										<button type="button" className="btn btn-outline-dark">
+											More information
+										</button>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+					<div className="card mb-3" style={{ maxwidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src="https://picsum.photos/200/200" style={{ width: "200px", height: "200px" }} />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">
+										<b>Nombre del servicio</b>
+									</h5>
+									<p>
+										<b>Categoria</b>
+									</p>
+									<p>
+										<b>Ubicacion</b>
+									</p>
+									<p className="card-text">
+										<button type="button" className="btn btn-outline-dark">
+											More information
+										</button>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="row m-4">
+				<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+					<div className="card mb-3" style={{ maxwidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src="https://picsum.photos/200/200" style={{ width: "200px", height: "200px" }} />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">
+										<b>Nombre del servicio</b>
+									</h5>
+									<p>
+										<b>Categoria</b>
+									</p>
+									<p>
+										<b>Ubicacion</b>
+									</p>
+									<p className="card-text">
+										<button type="button" className="btn btn-outline-dark">
+											More information
+										</button>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+					<div className="card mb-3" style={{ maxwidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src="https://picsum.photos/200/200" style={{ width: "200px", height: "200px" }} />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">
+										<b>Nombre del servicio</b>
+									</h5>
+									<p>
+										<b>Categoria</b>
+									</p>
+									<p>
+										<b>Ubicacion</b>
+									</p>
+									<p className="card-text">
+										<button type="button" className="btn btn-outline-dark">
+											More information
+										</button>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+					<div className="card mb-3" style={{ maxwidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src="https://picsum.photos/200/200" style={{ width: "200px", height: "200px" }} />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">
+										<b>Nombre del servicio</b>
+									</h5>
+									<p>
+										<b>Categoria</b>
+									</p>
+									<p>
+										<b>Ubicacion</b>
+									</p>
+									<p className="card-text">
+										<button type="button" className="btn btn-outline-dark">
+											More information
+										</button>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="row m-4">
+				<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+					<div className="card mb-3" style={{ maxwidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src="https://picsum.photos/200/200" style={{ width: "200px", height: "200px" }} />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">
+										<b>Nombre del servicio</b>
+									</h5>
+									<p>
+										<b>Categoria</b>
+									</p>
+									<p>
+										<b>Ubicacion</b>
+									</p>
+									<p className="card-text">
+										<button type="button" className="btn btn-outline-dark">
+											More information
+										</button>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+					<div className="card mb-3" style={{ maxwidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src="https://picsum.photos/200/200" style={{ width: "200px", height: "200px" }} />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">
+										<b>Nombre del servicio</b>
+									</h5>
+									<p>
+										<b>Categoria</b>
+									</p>
+									<p>
+										<b>Ubicacion</b>
+									</p>
+									<p className="card-text">
+										<button type="button" className="btn btn-outline-dark">
+											More information
+										</button>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
+					<div className="card mb-3" style={{ maxwidth: "540px" }}>
+						<div className="row g-0">
+							<div className="col-md-4">
+								<img src="https://picsum.photos/200/200" style={{ width: "200px", height: "200px" }} />
+							</div>
+							<div className="col-md-8">
+								<div className="card-body">
+									<h5 className="card-title">
+										<b>Nombre del servicio</b>
+									</h5>
+									<p>
+										<b>Categoria</b>
+									</p>
+									<p>
+										<b>Ubicacion</b>
+									</p>
+									<p className="card-text">
+										<button type="button" className="btn btn-outline-dark">
+											More information
+										</button>
+									</p>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 
 			{/* // aca empieza el sector de anuncios */}
 
-			{/* <div className="container">
-				<div className="row text-center">
+			{/* <div className="row text-center">
 					<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
 						<div className="thumbnail">
-							{" "}
 							<img
-								src="https://picsum.photos/200/300"
+								src="https://picsum.photos/200/200"
 								alt="Thumbnail Image 1"
-								className="img-responsive"
+                                className="img-responsive"
+                                style={{ width: "250px", height: "250px" }}
 							/>
 							<div className="caption">
 								<h3>Product</h3>
@@ -371,8 +379,7 @@ export const Home = () => {
 							</div>
 						</div>
 					</div>
-				</div>
-			</div> */}
+				</div> */}
 		</div>
 	);
 };
