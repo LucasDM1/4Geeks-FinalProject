48260873ae03fba46efa8b3cab65bfbd94a6e3ce
María Celeste Alpízar Solano <maria.alpizar.72@gmail.com>
2021-04-23 03:10:39 +0000

    Carousel ready but  not work

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,83 +8,249 @@ export const Home = () => {
 
 	return (
 		<div className="container-fluid">
-			<div className="row">
-				<div className="col-12">
-					<div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+			<div className="row justify-content-center">
+				<div className="col-10">
+					<div
+						id="carouselExampleIndicators"
+						className="carousel slide"
+						data-bs-ride="carousel"
+						style={{ backgroundColor: "gray" }}>
+						<ol className="carousel-indicators">
+							<li
+								data-target="#carouselExampleIndicators"
+								data-slide-to="0"
+								className="active"
+								style={{
+									borderRadius: "50%",
+									width: "20px",
+									height: "20px",
+									backgroundColor: "black",
+									opacity: "0.25"
+								}}
+							/>
+							<li
+								data-target="#carouselExampleIndicators"
+								data-slide-to="1"
+								style={{
+									borderRadius: "50%",
+									width: "20px",
+									height: "20px",
+									backgroundColor: "black",
+									opacity: "0.25"
+								}}
+							/>
+							<li
+								data-target="#carouselExampleIndicators"
+								data-slide-to="2"
+								style={{
+									borderRadius: "50%",
+									width: "20px",
+									height: "20px",
+									backgroundColor: "black",
+									opacity: "0.25"
+								}}
+							/>
+						</ol>
 						<div className="carousel-inner">
 							<div className="carousel-item active">
+								<div className="container" alt="First slide">
+									<div className="row p-1">
+										<div className="col-sm-12 col-lg-3 ">
+											<img
+												src="https://picsum.photos/250"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title text-center">Card title</h5>
+											</div>
+										</div>
+										<div className="col-sm-12 col-lg-3 ">
+											<img
+												src="https://picsum.photos/250"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title text-center">Card title</h5>
+											</div>
+										</div>
+										<div className="col-sm-12 col-lg-3 ">
+											<img
+												src="https://picsum.photos/250"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title text-center">Card title</h5>
+											</div>
+										</div>
+										<div className="col-sm-12 col-lg-3 ">
+											<img
+												src="https://picsum.photos/250"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title text-center">Card title</h5>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="carousel-item">
+								<div className="container" alt="Second slide">
+									<div className="row p-1">
+										<div className="col-sm-12 col-lg-3 ">
+											<img
+												src="https://neilpatel.com/wp-content/uploads/2017/08/colors.jpg"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title">Card title</h5>
+											</div>
+										</div>
+										<div className="col-sm-12 col-lg-3 ">
+											<img
+												src="https://neilpatel.com/wp-content/uploads/2017/08/colors.jpg"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title">Card title</h5>
+											</div>
+										</div>
+										<div className="col-sm-12 col-lg-3 ">
+											<img
+												src="https://neilpatel.com/wp-content/uploads/2017/08/colors.jpg"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title">Card title</h5>
+											</div>
+										</div>
+										<div className="col-sm-12 col-lg-3 ">
+											<img
+												src="https://neilpatel.com/wp-content/uploads/2017/08/colors.jpg"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title">Card title</h5>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="carousel-item">
 								<div className="container">
-									<div className="row">
-										<div className="col-sm-12 col-lg-4">
-											<div className="card bg-dark text-white" style={{ width: "300px" }}>
-												<img
-													src="https://picsum.photos/200/300"
-													className="card-img"
-													alt="..."
-												/>
-												<div className="card-img-overlay">
-													<h5 className="card-title">Card title</h5>
-													<p className="card-text">
-														This is a wider card with supporting text below as a natural
-														lead to additional content. This content is a little bit longer.
-													</p>
-													<p className="card-text">Last updated 3 mins ago</p>
-												</div>
+									<div className="row p-1">
+										<div className="col-sm-12 col-lg-3">
+											<img
+												src="https://www.hola.com/imagenes/cocina/recetas/20201009176964/pan-de-leche-thermomix-casero/0-876-239/receta-pan-thermomix-bollo-leche-m.jpg"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title text-dark text-center">Card title</h5>
 											</div>
 										</div>
-										<div className="col-sm-12 col-lg-4">
-											<div className="card bg-dark text-white" style={{ width: "300px" }}>
-												<img
-													src="https://picsum.photos/200/300"
-													className="card-img"
-													alt="..."
-												/>
-												<div className="card-img-overlay">
-													<h5 className="card-title">Card title</h5>
-													<p className="card-text">
-														This is a wider card with supporting text below as a natural
-														lead to additional content. This content is a little bit longer.
-													</p>
-													<p className="card-text">Last updated 3 mins ago</p>
-												</div>
+										<div className="col-sm-12 col-lg-3">
+											<img
+												src="https://www.hola.com/imagenes/cocina/recetas/20201009176964/pan-de-leche-thermomix-casero/0-876-239/receta-pan-thermomix-bollo-leche-m.jpg"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title text-dark text-center">Card title</h5>
 											</div>
 										</div>
-										<div className="col-sm-12 col-lg-4">
-											<div className="card bg-dark text-white" style={{ width: "300px" }}>
-												<img src="https://picsum.photos/200/300" className="card-img" />
-												<div className="card-img-overlay">
-													<h5 className="card-title">Card title</h5>
-													<p className="card-text">
-														This is a wider card with supporting text below as a natural
-														lead to additional content. This content is a little bit longer.
-													</p>
-													<p className="card-text">Last updated 3 mins ago</p>
-												</div>
+										<div className="col-sm-12 col-lg-3">
+											<img
+												src="https://www.hola.com/imagenes/cocina/recetas/20201009176964/pan-de-leche-thermomix-casero/0-876-239/receta-pan-thermomix-bollo-leche-m.jpg"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title text-dark text-center">Card title</h5>
+											</div>
+										</div>
+										<div className="col-sm-12 col-lg-3">
+											<img
+												src="https://www.hola.com/imagenes/cocina/recetas/20201009176964/pan-de-leche-thermomix-casero/0-876-239/receta-pan-thermomix-bollo-leche-m.jpg"
+												className="card-img"
+												style={{
+													width: "250px",
+													height: "250px"
+												}}
+											/>
+											<div className="card-img-overlay">
+												<h5 className="card-title text-dark text-center">Card title</h5>
 											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-						<button
-							className="carousel-control-prev p-0"
-							type="button"
-							data-bs-target="#carouselExampleControls"
-							data-bs-slide="prev">
+						<a
+							className="carousel-control-prev"
+							role="button"
+							href="#carouselExampleIndicators"
+							data-slide="prev"
+							style={{ width: "40px" }}>
 							<span className="carousel-control-prev-icon" aria-hidden="true" />
-							<span className="visually-hidden">Previous</span>
-						</button>
-						<button
-							className="carousel-control-next p-0"
-							type="button"
-							data-bs-target="#carouselExampleControls"
-							data-bs-slide="next">
+							<span className="visually-hidden" />
+						</a>
+						<a
+							className="carousel-control-next"
+							role="button"
+							href="#carouselExampleIndicators"
+							data-slide="next"
+							style={{ width: "40px" }}>
 							<span className="carousel-control-next-icon" aria-hidden="true" />
-							<span className="visually-hidden">Next</span>
-						</button>
+							<span className="visually-hidden" />
+						</a>
 					</div>
 				</div>
 			</div>
+
+			{/* // aca empieza el sector de anuncios */}
+
 			{/* <div className="container">
 				<div className="row text-center">
 					<div className="col-sm-4 col-md-4 col-lg-4 col-xs-6">
