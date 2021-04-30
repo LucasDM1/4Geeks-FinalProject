78806b1c0a7a02cce7d9feed4ead22ccd3185ba4
DC0507 <dcalderon179105@gmail.com>
2021-04-30 04:01:07 +0000

    login.js edited

--- migrations/versions/a2d2124b2489_.py
+++ migrations/versions/a2d2124b2489_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 300669371710
+Revision ID: a2d2124b2489
 Revises: 
-Create Date: 2021-04-30 02:03:09.666080
+Create Date: 2021-04-30 03:12:20.871527
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '300669371710'
+revision = 'a2d2124b2489'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,82 +1,93 @@
 import React, { useState, useEffect, useContext } from "react";
+import { useHistory } from "react-router";
 import { Context } from "../store/appContext";
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
 	const [email, setEmail] = useState(null);
 	const [password, setPassword] = useState(null);
-
+	const history = useHistory();
 	console.log("This is your token: ", store.token);
 	const handleClick = () => {
-		actions.login(email, password);
+		actions.login(email, password).then(resp => {
+			if (store.token !== null && store.token !== undefined) {
+				history.push("/");
+			} else {
+				null;
+			}
+		});
 	};
 	return (
 		<div className="container-fluid pt-3" style={{ margin: "6rem 0 8rem 0" }}>
-			<div className="col-md-5 mx-auto">
-				<div className="card">
-					<div className="card-body">
-						<div id="first">
-							<div className="myform form">
-								<div className="logo mb-3">
-									<div className="col-md-12 text-center">
-										<h1>Iniciar sesión</h1>
+			{store.token !== null && store.token !== undefined ? (
+				<h1>You are being redirected!!</h1>
+			) : (
+				<div className="col-md-5 mx-auto">
+					<div className="card">
+						<div className="card-body">
+							<div id="first">
+								<div className="myform form">
+									<div className="logo mb-3">
+										<div className="col-md-12 text-center">
+											<h1>Iniciar sesión</h1>
+										</div>
 									</div>
+									<form action="" method="post" name="login">
+										<div className="form-group">
+											<label>Correo electrónico</label>
+											<input
+												type="email"
+												name="email"
+												className="form-control"
+												id="email"
+												aria-describedby="emailHelp"
+												placeholder="Indique su correo electronico"
+												value={email}
+												onChange={e => setEmail(e.target.value)}
+											/>
+										</div>
+										<div className="form-group">
+											<label>Contraseña</label>
+											<input
+												type="password"
+												name="password"
+												id="password"
+												className="form-control"
+												aria-describedby="emailHelp"
+												placeholder=""
+												value={password}
+												onChange={e => setPassword(e.target.value)}
+											/>
+										</div>
+										<div className="form-group">
+											<p className="text-center">
+												Al registrase o loguearse usted esta aceptando nuestros{" "}
+												<a href="#">Terminos y Condiciones</a>
+											</p>
+										</div>
+										<div className="col-md-12 text-center ">
+											<button
+												type="button"
+												className=" btn btn-block mybtn btn-primary tx-tfm"
+												onClick={handleClick}>
+												Login
+											</button>
+										</div>
+										<div className="form-group">
+											<p className="text-center">
+												¿No estas registrado?{" "}
+												<a href="#" id="signup" onClick={() => history.push("/")}>
+													Registrate
+												</a>
+											</p>
+										</div>
+									</form>
 								</div>
-								<form action="" method="post" name="login">
-									<div className="form-group">
-										<label>Correo electrónico</label>
-										<input
-											type="email"
-											name="email"
-											className="form-control"
-											id="email"
-											aria-describedby="emailHelp"
-											placeholder="Indique su correo electronico"
-											value={email}
-											onChange={e => setEmail(e.target.value)}
-										/>
-									</div>
-									<div className="form-group">
-										<label>Contraseña</label>
-										<input
-											type="password"
-											name="password"
-											id="password"
-											className="form-control"
-											aria-describedby="emailHelp"
-											placeholder=""
-											value={password}
-											onChange={e => setPassword(e.target.value)}
-										/>
-									</div>
-									<div className="form-group">
-										<p className="text-center">
-											Al registrase o loguearse usted esta aceptando nuestros{" "}
-											<a href="#">Terminos y Condiciones</a>
-										</p>
-									</div>
-									<div className="col-md-12 text-center ">
-										<button
-											type="button"
-											className=" btn btn-block mybtn btn-primary tx-tfm"
-											onClick={handleClick}>
-											Login
-										</button>
-									</div>
-									<div className="form-group">
-										<p className="text-center">
-											¿No estas registrado?{" "}
-											<a href="#" id="signup">
-												Registrate
-											</a>
-										</p>
-									</div>
-								</form>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
